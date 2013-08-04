f0ec3cb9a2f0d20667471475019d2ea46967040e
Matthew Gifford <me@matthewgifford.com>
2013-08-04 03:51:16 -0700

    Increased the canvas size for switch to retina display.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function sketchProc(processing) {
-  processing.size(2000, 1500);
+  processing.size(3500, 2500);
   processing.background(75);
   for (var i = 0; i < processing.width; i++) {
     var b = Math.floor(Math.random()*50)+75,
-        w = Math.floor(Math.random()*250),
+        w = Math.floor(Math.random()*750),
         x = Math.floor(Math.random()*processing.width),
         y = Math.floor(Math.random()*processing.width);
     processing.strokeWeight(w);
