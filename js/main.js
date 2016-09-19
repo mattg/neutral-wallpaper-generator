b873f2758711b67165051a4bf859e10ece63bbd5
Matthew Gifford <mgifford@goldstar.com>
2016-09-18 21:20:26 -0700

    Make some code style updates

--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@ function sketchProc(processing) {
   processing.size(4000, 2500);
   processing.background(75);
   for (var i = 0; i < processing.width; i++) {
-    var b = Math.floor(Math.random()*50)+75,
-        w = Math.floor(Math.random()*750),
-        x = Math.floor(Math.random()*processing.width),
-        y = Math.floor(Math.random()*processing.width);
+    var b = Math.floor(Math.random()*50)+75;
+    var w = Math.floor(Math.random()*750);
+    var x = Math.floor(Math.random()*processing.width);
+    var y = Math.floor(Math.random()*processing.width);
     processing.strokeWeight(w);
     processing.stroke(b, 75);
     processing.line(y, 0, x, processing.height);
   }
 }
 
-var canvas = document.getElementById("drawing"),
-    processingInstance = new Processing(canvas, sketchProc);
-
+var canvas = document.getElementById('drawing');
+var processingInstance = new Processing(canvas, sketchProc);
